5ef28bea2f79918df7b5d24af750e0e3dadc2009
mpoeverlein <max.poeverlein@gmail.com>
2025-04-30 16:18:47 +0200

    use planeorigin instead of origin for planoconvec construction

--- lenses.cpp
+++ lenses.cpp
@@ -18,24 +18,12 @@ Vector SphericalLens::getOrigin() { return origin; }
 double SphericalLens::getRadius() { return radius; }
 double SphericalLens::getRefractiveIndex() { return refractiveIndex; }
 
-std::ostream& operator<<(std::ostream& os, const SphericalLens& l) {
-    os << "Lens: Origin: " << l.origin << " Radius: " << l.radius << " n: " << l.refractiveIndex << "\n";
-    return os;
-}
-
 double SphericalLens::detectCollisionTime(const Ray& ray) const {
    if (ray.energyDensity < Config::MIN_ENERGY_DENSITY) { return Inf; }
    return calculateCollisionTime(ray.origin, ray.direction, origin, radius);
 }
 
 std::vector<Ray> SphericalLens::createNewRays (const Ray& ray) const {
-    // std::vector<Ray> Ray::createReflectionAndRefraction (Vector surfaceNormal, Vector rotationAxis, double n2) {
-    /* Create reflection and refraction rays according to Snell's law
-    * n1 * sin(theta1) = n2 * sin(theta2)
-    * surfaceNormal: direction of surface normal
-    * rotationAxis: new rays created based on rotation of old about rotation axis
-    * n2: refractive index of other medium (n1 is stored in the Ray object)
-    */
     std::vector<Ray> newRays;
     Vector surfaceNormal;
     double n2;
@@ -48,23 +36,8 @@ std::vector<Ray> SphericalLens::createNewRays (const Ray& ray) const {
         surfaceNormal = (ray.end - origin).normalized();
         n2 = 1.;
     }
-    // Vector rotationAxis = ray.direction.cross(surfaceNormal).normalized();
 
-    return ::createNewRays(ray, surfaceNormal, n2, 0);
-
-    // double theta1 = angle(surfaceNormal, ray.direction);
-    // double theta2 = refractiveIndex / n2 * sin(theta1);
-    // // we create a new direction for the ray by rotating
-    // // the old direction by theta2-theta1
-    // double dtheta = theta2 - theta1;
-    // Vector refractionDirection = rotateVectorAboutAxis(ray.direction, rotationAxis, -dtheta);
-    // newRays.push_back(Ray(ray.end, refractionDirection, ray.energyDensity*0.98, n2));
-
-    // // create reflection
-    // Vector reflectionDirection = rotateVectorAboutAxis(ray.direction, rotationAxis, -(M_PI-2*theta1));
-    // newRays.push_back(Ray(ray.end, reflectionDirection, ray.energyDensity*0.02, refractiveIndex));
-    
-    // return newRays;
+    return ::createNewRays(ray, surfaceNormal, n2, 0); // reflectance set to 0
 }
 
 
@@ -75,15 +48,23 @@ std::string SphericalLens::forPythonPlot() const {
     return oss.str();
 }
 
-PlanoConvex::PlanoConvex(Vector origin_, double radius_, double n_, Vector height_) {
-    origin = origin_;
+
+////////////////////////////////
+
+
+    
+// PlanoConvex::PlanoConvex(Vector origin_, double radius_, double n_, Vector height_) {
+PlanoConvex::PlanoConvex(Vector planeOrigin_, double radius_, double n_, Vector height_) {
+    planeOrigin = planeOrigin_;
     radius = radius_;
     refractiveIndex = n_;
     height = height_;
     if (height.magnitude() > radius) {
         throw "Height vector must not be longer than sphere radius!";
     }
-    planeOrigin = origin + (radius - height.magnitude()) * height.normalized();
+    // planeOrigin = origin + (radius - height.magnitude()) * height.normalized();
+    origin = planeOrigin + height - radius * height.normalized();
+    std::cout << " PLANE ORIGIN " << planeOrigin << "\n";
     planeRadius = 2 * radius * height.magnitude() - height.magnitude()*height.magnitude();
     apex = origin + radius * height.normalized();
     openingAngle = acos(planeRadius / radius);
@@ -176,4 +157,15 @@ std::vector<Ray> createNewRays (const Ray& ray, Vector surfaceNormal, double n2,
     newRays.push_back(Ray(ray.end, reflectionDirection, ray.energyDensity*reflectance, n1));
     
     return newRays;
+}
+
+
+
+
+
+
+
+std::ostream& operator<<(std::ostream& os, const SphericalLens& l) {
+    os << "Lens: Origin: " << l.origin << " Radius: " << l.radius << " n: " << l.refractiveIndex << "\n";
+    return os;
 }
