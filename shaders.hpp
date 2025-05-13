ab303eb61061ac05461365ef71067c502863249b
mpoeverlein <max.poeverlein@gmail.com>
2025-05-13 10:52:36 +0200

    only use 3d pos and color in vertex shader

--- shaders.hpp
+++ shaders.hpp
@@ -1,14 +1,13 @@
 static const char* vertex_shader_text =
 "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
 "uniform mat4 MVP;\n"
-"in vec3 vCol;\n"
-"in vec2 vPos;\n"
 "out vec3 color;\n"
 "void main()\n"
 "{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
+"    gl_Position = MVP * vec4(aPos, 1.0);\n"
+"    color = aColor;\n"
 "}\n";
  
 static const char* fragment_shader_text =
