37afff48a04e2c9ed80228e51934cba6ba79bfda
mpoeverlein <max.poeverlein@gmail.com>
2025-05-23 15:13:17 +0200

    add virtual toString

--- optdev.hpp
+++ optdev.hpp
@@ -15,6 +15,7 @@ class OpticalDevice {
     virtual std::vector<Ray> createNewRays(const Ray& ray) const = 0;
     virtual std::string forPythonPlot() const = 0;
     virtual void createGraphicVertices(std::vector<Vertex>& vertices, std::vector<unsigned int>& indices) const = 0;
+    virtual std::string toString() const = 0;
 };
 
 #endif /* optdev_HPP */
